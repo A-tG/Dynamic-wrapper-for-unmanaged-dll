cd52d25a6a727faa745b4b2fc0251fb584460373
A-tG <a3tg.developer@gmail.com>
2021-04-27 16:36:08 +0300

    added public modifier

--- dll wrapper base/DllWrapperBase partial/DllWrapperBase.cs
+++ dll wrapper base/DllWrapperBase partial/DllWrapperBase.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 namespace AtgDev.Utils.Native
 {
-    abstract partial class DllWrapperBase
+    public abstract partial class DllWrapperBase
     {
         private IntPtr m_dllHandle;
 
